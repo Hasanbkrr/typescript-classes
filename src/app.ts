55d0aaadffa971ff832bcc902606559a7373df66
hasan colak <hasan.colak1@ogr.gelisim.edu.tr>
2024-06-07 09:52:04 +0300

    Yeni branch üzerinde yapılan değişiklikler

--- src/app.ts
+++ src/app.ts
@@ -1,142 +1,16 @@
 
-
-/*const button =document.querySelector('button')!;
-button.addEventListener('click', () =>{
-    console.log("clicked");
-});
-type toplanabilir = 'toplama' | 'cikarma' | 'carpma' | 'bolme';
-//hesap makinesi 
-function hesapmakinesi(number1 : number, number2: number,islemsecme :toplanabilir){
-
-if(islemsecme == 'toplama'){
-    return number1 + number2;
-}
-if(islemsecme == 'cikarma'){
-    return number1 - number2;
-}
-if(islemsecme == 'carpma'){
-    return number1 * number2;
-}
-if(islemsecme == 'bolme'){
-    return number1 / number2;
-}
-
-}
-/*
-// const and let 
-const diff = "hasan";
-diff = "bekir";
-let change ="hasan";
-change = "bekir";
-//  var is global value
-var bekir = "agasf"
-
-console.log(hesapmakinesi(50,20,'bolme'));     
- */
-/*
-//2 ya da daha az variable var ise bu fonksiyon çalışır
-const add = (a:number , b : number)  => {
-    return a+b;
-}
-
-console.log(add(2,5));
-*/
-/*
-//üslü sayılar fonksiyonu
-function uslusayilar(numbr1:number,numbr2:number){
-    let result = 1;
-    for(let i=1; i<=numbr2; i++){  
-       
-        result = numbr1 *result;  
-        console.log(result)
-        if(i>=numbr2){
-            break;
-        }
-    }
-       
-    }
-    uslusayilar(2,6)
-    //push method 
-    //spread operator 
-    const hobbi=["spor", "basketbol"];
-    const aktifhobbi=["yürüme"];
-    aktifhobbi.push(...hobbi);
-    console.log(aktifhobbi);
-
-    const person ={
-        ad:"hasan",
-        age :21
-    };
-    const coppiedperson ={...person};
-    console.log(coppiedperson);
-    
-    //rest paramaters
-
-    const add = (...numbers : number[]) =>{
-        let results = 0;
-        return numbers.reduce((curResult, curValue) =>{
-            return curResult + curValue;
-        },0);
-    };
-        
-    
-    //object destructing
-    const addednumbers = add(5,10,2,3.7);   
-    console.log(addednumbers);
-
-    
-    const [hobby1, hobby2, ...remaininghobbi] = hobbi;
-    console.log(hobbi,hobby1,hobby2);
-
-    const {ad :username , age} = person;
-    console.log(username,age);
-*/
-/*
-//classes
-//private - public 
-class Departmen {
-    name : string;
-    private employees :string[]=[];
-    constructor(n: string) {
-        this.name = n;
-
-    }
-    describe(this: Departmen){
-        console.log('departman: '+ this.name);
-    }
-    addemployee(employee:string){
-        this.employees.push(employee);
-    }
-    printemployeeinfo(){
-        console.log(this.employees.length);
-        console.log(this.employees);
-    }
-}
-
-
-
-const accounting = new Departmen('accounting');
-accounting.addemployee('max');
-accounting.addemployee('hasan');
-accounting.printemployeeinfo();
-accounting.describe();
-*/
-//const accountingCopy ={ name : 's' ,describe: accounting.describe};
-//accountingCopy.describe();
-
-//import { errors } from "undici-types";
-
-//shortcut to class
-class Department {
+//abstract class
+abstract class Department {
+    static yil=2020;
     //private -- protected 
     protected employees :string[]=[];
-    constructor(private readonly id: string, public name: string) {
-        
+    constructor(protected readonly id: string, public name: string) {
+        console.log(Department.yil);
 
     }
-    describe(this: Department){
-        console.log('departman: '+ this.name);
-    }
+    //abstract 
+    abstract describe(this: Department):void;
+    
     addemployee(employee:string){
         this.employees.push(employee);
     }
@@ -144,6 +18,9 @@ class Department {
         //this.id ='d2';  --read only so you cant change it
         console.log(this.employees.length);
         console.log(this.employees);
+    }
+    static createemployee(name: string){
+        return {name: name};
     }
 }
 //inheritance
@@ -154,7 +31,12 @@ class ITDepartman extends Department
         super(id,'IT');
         this.admins = admins;
     }
-}
+    describe(){
+        console.log('It department ID:'+ this.id)
+    }
+        
+    }
+
 //get and set 
 const accounting = new ITDepartman('d1',['hasan']);
 accounting.addemployee('maxs');
@@ -164,6 +46,7 @@ console.log(ITDepartman);
 
 class accountingdepartment extends Department{
 private lastreport:string;
+private static instance :accountingdepartment;
 get mostrecentreport (){
     if (this.lastreport){
         return this.lastreport;
@@ -179,9 +62,17 @@ set mostrecentreport(value: string){
     this.addreport(value)
 
 }
-constructor(id:string, private reports : string[]){
+//private constructors
+private constructor(id:string, private reports : string[]){
     super(id , 'IT');
     this.lastreport =reports[0];
+}
+static getinstance(){
+    if (this.instance){
+        return this.instance;
+    }
+    this.instance =new accountingdepartment('d2',[]);
+    return this.instance;
 }
 addreport(text:string){
     this.reports.push(text);
@@ -189,13 +80,63 @@ addreport(text:string){
 printreports(){
     console.log(this.reports);
 }
+describe(){
+console.log('accounting departmen- ID:' + this.id);    
+}
 
 }
-const accountingg = new accountingdepartment('d2',[]);
+const accounting1 = accountingdepartment.getinstance();
+const accounting2 = accountingdepartment.getinstance();
+console.log(accounting1, accounting2);
+accounting1.addreport('something worng');
 
-accountingg.addreport('something worng');
+accounting1.mostrecentreport ='year reports:';
+accounting1.printreports();
+ //static values 
+const employee1 = Department.createemployee('shasn');
 
-accountingg.mostrecentreport ='year reports:';
-accountingg.printreports();
- 
+accounting1.describe();
+//interfaces 
+interface named {
+    readonly name?: string;
+    outputname?: string;
+}
+interface Addfn{
+    (a:number , b:number) : number;
+}
+interface greettings extends named{
+    
+    
+    greet(phrase : string) :void;
+}
+class person implements greettings {
+    name?: string;
+    constructor(n?:string){
+        if(n){
+        this.name = n;
+        }
+    }
+    greet(phrase:string ){
+        if (this.name){
+        console.log(phrase + ' ' + this.name);
+        }
+        else{
+            console.log('hi');
+        }
+    }
+    }
 
+
+let user1:greettings;
+user1 = {
+    
+    name :'hasan',
+    
+    greet(phrase:string ){
+        console.log(phrase +'' +this.name);
+    }
+};
+user1.greet('hello there im  '); 
+user1 = new person('hasan');
+console.log(user1);
+// ? at the end is means optional
